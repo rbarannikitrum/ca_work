244ace1083835a1ad96d3bf25cd3ce886f196f33
rbarannikitrum <rbarannik.itrum@gmail.com>
2023-03-09 22:39:01 +0300

    end task2

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
 const loginData = {
   login: "user",
   password: "1234",
-};
+}
 
 app.post("/login", (request, response) => {
-  if (!request.body) return response.status(400).send("no body");
+  console.log(request.body)
+  if (!request.body) return response.status(400).send("no body")
   if (
     request.body.login === loginData.login &&
     request.body.password === loginData.password
   ) {
-    response.send(JSON.stringify({ msg: "success" }));
-  } else response.send(JSON.stringify({ msg: "failed" }));
-});
+    response.status(200).send(JSON.stringify({ msg: "success" }))
+  } else response.status(400).send(JSON.stringify({ msg: "failed" }))
+})
 
-app.listen(8080, () => console.log("Сервер запущен..."));
+app.listen(8080, () => console.log("Сервер запущен..."))
--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,36 @@
-const login = document.querySelector("#login").value;
-const password = document.querySelector("#password").value;
-
 async function sendData() {
+  const login = document.querySelector("#login").value
+  const password = document.querySelector("#password").value
   const sendObj = {
     login: login,
     password: password,
-  };
+  }
+  console.log(JSON.stringify(sendObj))
   await fetch("http://localhost:8080/login", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(sendObj),
+  }).then((res) => {
+    res.status === 200 ? setMain() : setError()
   })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
 }
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => sendData());
+function setMain() {
+  document.querySelector("button").remove()
+  document.querySelector("#login").remove()
+  document.querySelector("#password").remove()
+  const main = document.createElement("span")
+  main.innerText = "Главная"
+  document.querySelector("div").appendChild(main)
+}
+
+function setError() {
+  const main = document.createElement("span")
+  main.innerText = "Неверные данные"
+  document.querySelector("div").appendChild(main)
+}
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => sendData())
