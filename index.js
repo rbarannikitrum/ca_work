4c4f24a46494ab289dfd14925d07c2099b5dee05
rbarannikitrum <rbarannik.itrum@gmail.com>
2023-03-11 16:52:21 +0300

    refactor from regexp to functions

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const submit = document.querySelector('#submit')
 submit.addEventListener('click', () => validateAndSend())
-const regexpEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const rexexpPhone = /\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})? ?(\w{1,10}\s?\d{1,6})?/
 const info = document.createElement('div')
 let form = document.forms.data
 
 function validateAndSend() {
     console.log(form.elements.name.value, form.elements.surname.value, form.elements.email.value, form.elements.phone.value)
-    if (form.elements.name.value
-        && form.elements.surname.value
-        && regexpEmail.test(form.elements.email.value)
-        && rexexpPhone.test(form.elements.phone.value)) {
+    if (form.elements.name.value.trim()
+        && form.elements.surname.value.trim()
+        && checkValidEmail(form.elements.email.value)
+        && checkValidPhone(form.elements.phone.value)) {
             /// логика отправки на бекенд
         document.querySelector('form').reset()
         info.style.border = '2px dashed green'
@@ -21,4 +19,18 @@ function validateAndSend() {
         info.innerText = 'Введены неккорректные данные'
         form.appendChild(info)
     }
+}
+
+function checkValidEmail(email) {
+    const emailArr = email.split('')
+    const firstIndex = emailArr.indexOf('@')
+    const secondIndex = emailArr.lastIndexOf('.')
+    return firstIndex > 0 && secondIndex > firstIndex
+}
+
+function checkValidPhone (phone) {
+    const phoneArr = phone.split('')
+    const condition1 = phoneArr.length === 11 && phoneArr[0] === '8'
+    const condition2 = phoneArr.length === 12 && phoneArr[0] === '+' && phoneArr[1] === '7'
+    return condition1 || condition2
 }
